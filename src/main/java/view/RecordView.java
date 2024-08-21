80fb1d3745779817c8984c238c10e25fe8445ad3
LouiIII3 <jikdsunghuilee@naver.com>
2024-08-21 14:59:14 +0900

    내역 날짜 추가

--- src/main/java/view/RecordView.java
+++ src/main/java/view/RecordView.java
@@ -2,12 +2,22 @@ package view;
 
 import domain.Record;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 public class RecordView {
 
+    LocalDate currentDate = LocalDate.now();
+    // 현재 날짜에서 60일 전 날짜를 계산
+    LocalDate startDateLocal = currentDate.minus(60, ChronoUnit.DAYS);
+
+
     public void printRecords(List<Record> records) {
+        System.out.println("[입출금 내역 조회: " + startDateLocal + "~ " + currentDate + "]|");
         System.out.println("-----------------------------------------");
+
+
         for (Record record : records) {
             if (record.getType().equals("입금")) {
                 System.out.println("| 입금 내역                              |");
